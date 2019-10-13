064a90eb6f3dea0b1dd342ffc1a00f275fc50fad
David Soto <augusto.soto@udea.edu.co>
2019-10-13 18:22:13 -0500

    add docker

--- dockerfile
+++ dockerfile
@@ -2,4 +2,7 @@ FROM node:10
 
 WORKDIR /app
 
+EXPOSE 3000
+
 CMD ["npm", "start"]
+
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   },
 
   devServer: {
-    port: 9000
+    host: '0.0.0.0',
+    port: 3000
   },
 
   module: {
